81f3eca1471fb0688959b22b478edd87acf74b4e
RobertXing <rxing24@uwo.ca>
2025-03-03 09:14:18 -0500

    feat: airframe scraper

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from src.seatguru import ConfigurationFinder
+from src.airframes import AirframeFinder
 
 def main():
+    seatguru = ConfigurationFinder()
+    airframes = AirframeFinder()
+
     operator = input("Operator: ")
     aircraft = input("Aircraft: ")
     registration = input("Registration: ")
-    seatguru = ConfigurationFinder()
-    seats = seatguru.scrape(operator, aircraft)
-    print(f"{operator} {aircraft} {registration} configured with {seats} seats")
+    
+    #seats = seatguru.scrape(operator, aircraft)
+    #print(f"{operator} {aircraft} {registration} configured with {seats} seats")
+    airframe = airframes.scrape(registration)
+    print(f"{airframe["Registration"]} found")
+    print(f"Model: {airframe["Model"]}")
+    print(f"Type: {airframe["Type"]}")
+    print(f"SELCAL: {airframe["Selcal"]}")
+    print(f"ICAO24: {airframe["ICAO24"]}")
 
 if __name__ == "__main__":
     main()
